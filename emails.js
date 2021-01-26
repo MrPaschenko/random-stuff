f0c22c8617240247b8d2f52a0c64be610bae09e9
Dmytro Pashchenko <paschenkod2@gmail.com>
2021-01-27 00:47:18 +0200

    crlf to lf

--- emails.js
+++ emails.js
@@ -4,7 +4,7 @@ const string1 = '12123@gmail.com, skghh@mail.ru, sfsdf@ya.ru, sdflkjsg@gmail.com
 
 const array1 = string1.split(',');
 
-for (let key in array1) {
+for (const key in array1) {
   array1[key] = array1[key].trim();
 }
 
@@ -15,8 +15,8 @@ const emails = array1.filter(value => value.includes('@'));
 const names = [];
 const domains = [];
 
-for (let key in emails) {
-  let temp = emails[key].split('@');
+for (const key in emails) {
+  const temp = emails[key].split('@');
   names.push(temp[0]);
   domains.push(temp[1]);
 
