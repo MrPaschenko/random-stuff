4414839cf7f15520f85a0443d2ebd2ce561d4304
Dmytro Pashchenko <paschenkod2@gmail.com>
2021-02-08 19:46:37 +0200

    comments

--- Codewars/Is this a triangle.js
+++ Codewars/Is this a triangle.js
@@ -4,5 +4,5 @@ function isTriangle(a, b, c) {
   return a + b > c && a + c > b && b + c > a;
 }
 
-console.log(isTriangle(1, 2, 2));
-console.log(isTriangle(7, 2, 2));
+console.log(isTriangle(1, 2, 2)); // true
+console.log(isTriangle(7, 2, 2)); // falser
